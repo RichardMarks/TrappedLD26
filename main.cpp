e65221f7c0c2735cdab91585312d02bbe545b66a
Richard Marks <ccpsceo@gmail.com>
2013-04-26 21:36:01 -0500

    project started

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Trapped LD26
+// A Ludum Dare 26 Compo Entry by Richard Marks (ccpsceo@gmail.com)
+// © Copyright 2013, Richard Marks, Bang Bang Attack Studios
+// www.ccpssolutions.com | www.bangbangattackstudios.com
+// ================================================================
+
+/*
+    Objective: Reach the edge of the screen to win the game.
+
+    Summary:
+
+    In Trapped LD26, you must attempt to reach the edge of the screen.
+    There are laser beams (red lines) that are blocking your way.
+    If you touch a laser beam, you die.
+    There are switches (green boxes) that you must walk on to change
+    the direction that the lasers are pointing. Careful, some switches
+    control multiple lasers, and you may get fried!
+
+    Controls:
+
+    WASD, Arrow Keys to move player
+    ESC to quit
+
+*/
